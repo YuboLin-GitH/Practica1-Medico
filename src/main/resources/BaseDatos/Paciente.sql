5fcde6afe4b27e7618639091a92287273198989a
yubo.lin <yubo.lin@educa.jcyl.es>
2025-09-29 11:47:51 +0200

    añadir SHA256

--- src/main/resources/BaseDatos/Paciente.sql
+++ src/main/resources/BaseDatos/Paciente.sql
@@ -13,14 +13,14 @@ telefono int(9)
  );
  
   INSERT INTO Paciente  VALUES
- (1,"12345678A","David","david","c/ AAA", 611222333),
- (2,"34564546B","Angel","angel","c/ BBB", 611512183),
- (3,"62145448C","Lucia","lucia","c/ CCC", 611224013),
- (4,"91321654D","Martina","martina","c/ DDD", 618434555),
- (5,"51248345E","Sofia","sofia","c/ EEE", 649161161),
- (6,"84345876F","Hugo","hugo","c/ FFF", 616713488),
- (7,"81431548G","Leo","leo","c/ GGG", 668453178),
- (8,"11501548H","Daniel","daniel","c/ HHH", 691246578);
+ (1,"12345678A","David",SHA2("david",256),"c/ AAA", 611222333),
+ (2,"34564546B","Angel",SHA2("angel",256),"c/ BBB", 611512183),
+ (3,"62145448C","Lucia",SHA2("lucia",256),"c/ CCC", 611224013),
+ (4,"91321654D","Martina",SHA2("martina",256),"c/ DDD", 618434555),
+ (5,"51248345E","Sofia",SHA2("sofia",256),"c/ EEE", 649161161),
+ (6,"84345876F","Hugo","SHA2(hugo",256),"c/ FFF", 616713488),
+ (7,"81431548G","Leo","SHA2(leo",256),"c/ GGG", 668453178),
+ (8,"11501548H","Daniel",SHA2("daniel",256),"c/ HHH", 691246578);
  
 
 CREATE TABLE Especialidad(
